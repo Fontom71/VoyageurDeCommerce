8fd348319c401633eaf26e3510fa8ef0a438829c
Fontom's <tomtombert7@gmail.com>
2022-04-12 10:55:56 +0200

    Base

--- modele/algorithmes/realisations/AlgorithmeCroissant.cs
+++ modele/algorithmes/realisations/AlgorithmeCroissant.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using VoyageurDeCommerce.modele.distances;
 using VoyageurDeCommerce.modele.lieux;
 
@@ -14,12 +15,17 @@ namespace VoyageurDeCommerce.modele.algorithmes.realisations
 
         public override void Executer(List<Lieu> listeLieux, List<Route> listeRoute)
         {
+            DateTimeOffset timeStart = DateTime.Now;
             FloydWarshall.calculerDistances(listeLieux, listeRoute);
             foreach (Lieu lieu in listeLieux)
             {
                 this.Tournee.Add(lieu);
                 this.NotifyPropertyChanged("Tournee");
             }
+            DateTimeOffset timeStop = DateTime.Now;
+            int time = (int)(timeStop.ToUnixTimeMilliseconds() - timeStart.ToUnixTimeMilliseconds());
+            Console.WriteLine(time);
+            MessageBox.Show("Latence: " + time.ToString() + " ms", "Latence de traitement", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
